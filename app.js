6199a70c380de4088451a87f70ebd35daa18b45c
Mahesh maharjan <mah.dot98@gmail.com>
2024-01-21 16:17:10 +0545

    Added login wtih jwt for both admin and user role

--- app.js
+++ app.js
@@ -43,38 +43,38 @@ app.post("/api/register", async (req, res) => {
   try {
       let existingUser;
       if (role === "admin") {
-          existingUser = await Admin.findOne({ username });
+          existingUser = await Admin.findOne({ email });
           if (existingUser) {
-              return res.status(400).send({ message: "Admin already exists" });
+              return res.status(400).send({ message: "Email already exists" });
           }
 
           const admin = new Admin({
               username,
               email,
               mobile,
-              password,
+              password: await bcrypt.hash(password, 10),
               address,
           });
 
           await admin.save();
           res.status(201).send({ message: "Admin created successfully" });
       } else if (role === "user") {
-          existingUser = await User.findOne({ username });
+          existingUser = await User.findOne({ email });
           if (existingUser) {
               return res.status(400).send({ message: "Email already exists" });
           }
-
           const user = new User({
               username,
               email,
               mobile,
-              password,
+              password: await bcrypt.hash(password, 10),
               address,
           });
 
-
-
+        await user.save();
+        res.status(201).send({ message: "User has been created successfully" });
       }
+      
   } catch (err) {
     console.error("Registration failed", err);
       res.status(400).send({ message: "Registration failed", error: err });
@@ -85,9 +85,9 @@ app.post("/api/send-otp", async (req, res) => {
   const { email } = req.body;
 
   try {
-    // Generate OTP for the first-time registration
+    /// Generate OTP for the first-time registration
     const otp = generateOtp();
-    // Send OTP via email
+    /// Send OTP via email
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
@@ -106,21 +106,115 @@ app.post("/api/send-otp", async (req, res) => {
   }
 });
 
+const generateAccessToken = (userId) => {
+  return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '300m' });
+};
 
- app.post("/api/login", async (req,res) =>{
-  const { email, password} = req.body;
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send({ message: "No access token provided" });
+  }
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findOne({ _id: decodedToken.userId });
+    if (!user) {
+      return res.status(401).send({ message: "User not found" });
+    }
+    req.user = { userId: user._id, username: user.username };
+    next();
+  } catch (err) {
+    return res.status(403).send({ message: "Invalid token" });
+  }
+};
+
+function generateAdminAccessToken(adminId) {
+  const payload = {
+    adminId,
+    role: "admin"
+  };
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '300m' });
+}
+
+const authenticateAdminToken = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send({ message: "No access token provided" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const admin = await Admin.findOne({ _id: decodedToken.adminId });
+    if (!admin) {
+      return res.status(401).send({ message: "Found no admin" });
+    }
+    req.admin = { adminId: admin._id, username: admin.username };
+    next();
+  } catch (err) {
+    return res.status(403).send({ message: "Invalid token" });
+  }
+};
+
+app.post('/api/token', (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    return res.status(401).send({ message: 'No refresh token provided' });
+  }
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+        return res.status(403).send({ message: 'Refresh token expired' });
+      } else {
+        return res.status(403).send({ message: 'Invalid refresh token' });
+      }
+    }
+    const accessToken = generateAccessToken(user.userId);
+    res.send({ accessToken });
+  });
+});
+
+app.post("/api/login", async (req,res) =>{
+  const { email, password, role} = req.body;
   let user;
-  user = await User.findOne({ email });
+  if (role === "user") {
+    user = await User.findOne({ email });
+  } else if (role === "admin") {
+    user = await Admin.findOne({ email });
+  }
   if (!user) {
-    return res.status(401).send({ message: "Username or password is incorrect" });
+    return res.status(401).send({ message: "User with email not found" });
   }
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
-    return res.status(401).send({ message: "Username or password is incorrect" });
+    return res.status(401).send({ message: " password is incorrect" });
   }
-  if (!user.confirmed) {  
-    return res.status(401).send({ message: "confirmation is incorrect" });
+  let accessToken;
+  if (role === "user") {
+    accessToken = generateAccessToken(user._id);
+  } else if (role === "admin") {
+    accessToken = generateAdminAccessToken(user._id);
   }
+  if (role === "user") {
+    const refreshToken = jwt.sign({ userId: user._id }, process.env.REFRESH_TOKEN_SECRET);
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      path: "/api/token",
+    });
+    res.status(201).send({ accessToken, userId: user._id, message: "User logged in successfully" });
+  }
+  else if (role === "admin") {
+    const refreshToken = jwt.sign({ adminId: user._id }, process.env.REFRESH_TOKEN_SECRET);
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      path: "/api/token",
+    });
+    res.status(201).send({ accessToken, adminId: user._id, message: "admin logged in successfully" });
+  }
+
  })
 
 app.listen(3005, () => console.log("Server listening on port 3005"));
